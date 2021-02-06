28fcf63cc9a3b2c07305923d940ed4047b7e143d
James Howard <james.howard@mailchimp.com>
2021-02-06 18:33:50 -0500

    Add individual employee details

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const EmployeeCard = ({employee}) => {
+    console.log(employee);
     return (
-        <div>Employee</div>
+        <div>
+            <img src={employee.picture.medium} alt={`${employee.name.first} profile picture`}/>
+            <h2>{employee.name.first} {employee.name.last}</h2>
+            <h3>{employee.email} • {employee.cell}</h3>
+            <h4>{employee.gender}</h4>
+        </div>
     )
 }
 
